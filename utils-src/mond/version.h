31098cd0a7304d1c0bf2b6ab221608c2480be3a3
Satya <satya@cs.cmu.edu>
1995-10-09 19:27:12 +0000

    Reblurbed with new CMU and IBM notices for SOSP-15 CD-ROM

--- utils-src/mond/version.h
+++ utils-src/mond/version.h
@@ -1,19 +1,37 @@
 #ifndef _BLURB_
 #define _BLURB_
 /*
-                           CODA FILE SYSTEM
-                             Release 3.0
-                      School of Computer Science
-                      Carnegie Mellon University
-                          Copyright 1987-95
 
-This  code  may  be used outside Carnegie Mellon University only under
-the terms of the Coda license.  All other use is illegal.
+            Coda: an Experimental Distributed File System
+                             Release 3.1
+
+          Copyright (c) 1987-1995 Carnegie Mellon University
+                         All Rights Reserved
+
+Permission  to  use, copy, modify and distribute this software and its
+documentation is hereby granted,  provided  that  both  the  copyright
+notice  and  this  permission  notice  appear  in  all  copies  of the
+software, derivative works or  modified  versions,  and  any  portions
+thereof, and that both notices appear in supporting documentation, and
+that credit is given to Carnegie Mellon University  in  all  documents
+and publicity pertaining to direct or indirect use of this code or its
+derivatives.
+
+CODA IS AN EXPERIMENTAL SOFTWARE SYSTEM AND IS  KNOWN  TO  HAVE  BUGS,
+SOME  OF  WHICH MAY HAVE SERIOUS CONSEQUENCES.  CARNEGIE MELLON ALLOWS
+FREE USE OF THIS SOFTWARE IN ITS "AS IS" CONDITION.   CARNEGIE  MELLON
+DISCLAIMS  ANY  LIABILITY  OF  ANY  KIND  FOR  ANY  DAMAGES WHATSOEVER
+RESULTING DIRECTLY OR INDIRECTLY FROM THE USE OF THIS SOFTWARE  OR  OF
+ANY DERIVATIVE WORK.
+
+Carnegie  Mellon  encourages  users  of  this  software  to return any
+improvements or extensions that  they  make,  and  to  grant  Carnegie
+Mellon the rights to redistribute these changes without encumbrance.
 */
-
 
 static char *rcsid = "$Header$";
 #endif /*_BLURB_*/
+
 
 
 #ifndef VERSION_H
