eca662b2fbc3eaae3ebb928cb33e6873483f64de
Peter Braam <braam@cs.cmu.edu>
1996-11-22 19:20:09 +0000

    First Checkin (pre-release)

---
+++ lib-src/plumber/malloc.c
@@ -0,0 +1,7 @@
+/* ********************************************************************** *\
+ *         Copyright IBM Corporation 1988,1991 - All Rights Reserved      *
+ *        For full copyright information see:'andrew/config/COPYRITE'     *
+\* ********************************************************************** */
+static char *malloc_rcsid = "$Header$";
+
+#include "malloc.ci"
---
+++ lib-src/plumber/plumber.c
@@ -0,0 +1,7 @@
+/* ********************************************************************** *\
+ *         Copyright IBM Corporation 1988,1991 - All Rights Reserved      *
+ *        For full copyright information see:'andrew/config/COPYRITE'     *
+\* ********************************************************************** */
+static char *plumber_rcsid = "$Header$";
+
+#include "plumber.ci"
