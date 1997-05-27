1d010beaa0db1cb151d47e687f670769f9554b37
Peter Braam <braam@cs.cmu.edu>
1997-05-27 15:10:55 +0000

    *** empty log message ***

--- coda-src/volutil/vol-salvage.cc
+++ coda-src/volutil/vol-salvage.cc
@@ -1596,11 +1596,13 @@ PRIVATE int GetInodeSummary(char *path, VolumeId singleVolumeNumber)
     summaryFile = fdopen(summaryFd,"w");
     assert(summaryFile != NULL);
     nInodes = status.st_size / sizeof(ViceInodeInfo);
+#if 0
     if (nInodes == 0) {
 	LogMsg(0, VolDebugLevel, stdout, "%s vice inodes on %s; not salvaged", singleVolumeNumber? "No applicable": "No", dev);
 	close(summaryFd);
 	return(VNOVOL);
     }
+#endif
     ip = tmp_ip = (struct ViceInodeInfo *) malloc(status.st_size);
     if (ip == NULL) {
 	LogMsg(0, VolDebugLevel, stdout, "Unable to allocate enough space to read inode table; %s not salvaged", dev);
