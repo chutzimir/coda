fadbacf7c4a4d41d94fc0e21d0331bdc95d7d6e7
Peter Braam <braam@cs.cmu.edu>
1997-09-23 17:44:35 +0000

    Trivial changes for Linux sparc: source layout.

--- coda-src/resolution/reslock.cc
+++ coda-src/resolution/reslock.cc
@@ -91,7 +91,8 @@ extern "C" {
 
 
 long RS_LockAndFetch(RPC2_Handle RPCid, ViceFid *Fid,
-		     ResFetchType Request, ViceVersionVector *VV, ResStatus *rstatus,
+		     ResFetchType Request, ViceVersionVector *VV, 
+		     ResStatus *rstatus,
 		     RPC2_Integer *logsize, RPC2_Integer maxcomponents, 
 		     RPC2_Integer *ncomponents, ResPathElem *components) {
     LogMsg(1, SrvDebugLevel, stdout,
@@ -106,16 +107,6 @@ long RS_LockAndFetch(RPC2_Handle RPCid, ViceFid *Fid,
     *logsize = 0;
     int nentries  = 0;
 
-#ifdef 0
-    Keep this code for making fine granularity measurements; 
-    extern int clockFD;
-#define NSC_GET_COUNTER         _IOR('c', 1, long)
-    unsigned long startlockandfetch= 0;
-    unsigned long endlockandfetch = 0;
-    if (clockFD > 0) 
-	ioctl(clockFD, NSC_GET_COUNTER, &startlockandfetch);
-#endif 0
-
     // first set out parameters
     InitVV(VV);
     *logsize = 0;
@@ -126,13 +117,15 @@ long RS_LockAndFetch(RPC2_Handle RPCid, ViceFid *Fid,
     conninfo *cip = NULL;
     cip = GetConnectionInfo(RPCid);
     if (cip == NULL){
-	LogMsg(0, SrvDebugLevel, stdout,  "RS_LockAndFetch: Couldnt get conn info");
+	LogMsg(0, SrvDebugLevel, stdout,  
+	       "RS_LockAndFetch: Couldnt get conn info");
 	return(EINVAL);
     }
 
     // translate replicated VolumeId to rw id 
     if (!XlateVid(&Fid->Volume)){
-	LogMsg(0, SrvDebugLevel, stdout,  "RS_LockAndFetch: Couldnt translate Vid %x",
+	LogMsg(0, SrvDebugLevel, stdout,  
+	       "RS_LockAndFetch: Couldnt translate Vid %x",
 		Fid->Volume);
 	return(EINVAL);
     }
