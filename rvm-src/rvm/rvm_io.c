1f6aa91e6abc87bc4b34e8b70c636415839a93c9
Peter Braam <braam@cs.cmu.edu>
1997-10-15 15:44:09 +0000

    For Linux the raw_io flag for raw block devices was changed to false. The     fsync call will now give the same consistency guarantees as on other     platforms, and probably very efficiently since all writes will be     asynchronous and the kernel will flush all buffers for the device for     us.

--- rvm-src/rvm/rvm_io.c
+++ rvm-src/rvm/rvm_io.c
@@ -150,13 +150,12 @@ long set_dev_char(dev,dev_length)
     switch (mode)
         {
       case S_IFCHR:                     /* note raw io */
-#ifdef LINUX
-      case S_IFBLK:  /* in LINUX, the interface VFS to block and char
-                     * dev is the same, which means that no additional
-                     * character devices are required
-                     */
-#endif
         dev->raw_io = rvm_true;
+        break;
+      case S_IFBLK:  
+	  /* added for Linux. Linux supports fsync on block devices,
+      and such devices can be opened*/
+        dev->raw_io = rvm_false;
         break;
       case S_IFREG:
         dev->num_bytes = RVM_MK_OFFSET(0,
