b5f45dc911708a3d7e7937c5bc81cf6f89bf317d
Jan Harkes <jaharkes@cs.cmu.edu>
1998-10-03 16:59:25 +0000

    Robert found the (last?) uses of the cfs_ names.

--- coda-src/venus/venusvm.cc
+++ coda-src/venus/venusvm.cc
@@ -323,8 +323,8 @@ void VmonInit() {
     }
 #ifdef __BSD44__
 
-    RawStats[0].n_name = "_cfs_vfsopstats";
-    RawStats[1].n_name = "_cfs_vnodeopstats";
+    RawStats[0].n_name = "_coda_vfsopstats";
+    RawStats[1].n_name = "_coda_vnodeopstats";
     RawStats[2].n_name = 0;
     if (nlist(VMUNIX,RawStats) != 0) {
 	fprintf(stderr, "ERROR: running a pre-vfs-statistics kernel\n");
@@ -344,11 +344,11 @@ void VmonInit() {
 
     lseek(kmem, (long)RawStats[0].n_value, 0);
     read(kmem, (char *)vfsop_init_stats, 
-	 (int) (sizeof(struct cfs_op_stats)*CODA_VFSOPS_SIZE));
+	 (int) (sizeof(struct coda_op_stats)*CODA_VFSOPS_SIZE));
     
     lseek(kmem, (long)RawStats[1].n_value, 0);
     read(kmem, (char*)vnode_init_stats,
-	 (int)(sizeof(struct cfs_op_stats)*CODA_VNODEOPS_SIZE));
+	 (int)(sizeof(struct coda_op_stats)*CODA_VNODEOPS_SIZE));
 
     VmonSessionEventArraySize = (int) sizeof(VmonSessionEventArray);
 
@@ -648,12 +648,12 @@ static void CheckMC() {       // Check minicache stats
     if (Time - LastTime < VmonMiniCacheInterval) return;
     LastTime = Time;
 
-    struct cfs_op_stats vfsop_stats[CODA_VFSOPS_SIZE];
-    struct cfs_op_stats vnode_stats[CODA_VNODEOPS_SIZE];
+    struct coda_op_stats vfsop_stats[CODA_VFSOPS_SIZE];
+    struct coda_op_stats vnode_stats[CODA_VNODEOPS_SIZE];
 
     lseek(kmem, (long)RawStats[0].n_value, 0);
     read(kmem, (char *)vfsop_stats, 
-	 (int)(sizeof(struct cfs_op_stats)*CODA_VFSOPS_SIZE));
+	 (int)(sizeof(struct coda_op_stats)*CODA_VFSOPS_SIZE));
     
     for (i=0; i<CODA_VFSOPS_SIZE; i++) {
 	vfsop_stats[i].opcode = i;
@@ -665,7 +665,7 @@ static void CheckMC() {       // Check minicache stats
 
     lseek(kmem, (long)RawStats[1].n_value, 0);
     read(kmem, (char*)vnode_stats,
-	 (int)(sizeof(struct cfs_op_stats)*CODA_VNODEOPS_SIZE));
+	 (int)(sizeof(struct coda_op_stats)*CODA_VNODEOPS_SIZE));
 
     for (i=0; i<CODA_VNODEOPS_SIZE; i++) {
 	vnode_stats[i].opcode = i;
