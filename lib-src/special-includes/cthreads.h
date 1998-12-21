5c695fa23a387ee50aba49d38b7a150c555707c7
Jan Harkes <jaharkes@cs.cmu.edu>
1998-12-21 22:30:06 +0000

    Here it is.... GPL/LGPL

--- lib-src/special-includes/cthreads.h
+++ lib-src/special-includes/cthreads.h
@@ -1,3 +1,21 @@
+/* BLURB lgpl
+
+                           Coda File System
+                              Release 5
+
+          Copyright (c) 1987-1999 Carnegie Mellon University
+                  Additional copyrights listed below
+
+This  code  is  distributed "AS IS" without warranty of any kind under
+the  terms of the  GNU  Library General Public Licence  Version 2,  as
+shown in the file LICENSE. The technical and financial contributors to
+Coda are listed in the file CREDITS.
+
+                        Additional copyrights
+                           none currently
+
+#*/
+
 /* 
  * Mach Operating System
  * Copyright (c) 1989 Carnegie-Mellon University
@@ -7,6 +25,9 @@
 /*
  * HISTORY
  * $Log$
+ * Revision 4.2  1998/12/21 22:29:36  jaharkes
+ * Here it is.... GPL/LGPL
+ *
  * Revision 4.1  1997/01/08 21:54:22  rvb
  * r = 4.1; fix $ HEADERS
  *
