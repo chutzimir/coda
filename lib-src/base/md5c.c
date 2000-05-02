ba79810622845d6f93e4deba426fb26d829ede37
Jan Harkes <jaharkes@cs.cmu.edu>
2000-05-02 21:52:10 +0000

    Some more compilation errors in the new code fixed.

--- lib-src/base/md5c.c
+++ lib-src/base/md5c.c
@@ -198,10 +198,10 @@ MD5_Final (digest, context)
 	/* Pad out to 56 mod 64. */
 	index = (unsigned int)((context->count[0] >> 3) & 0x3f);
 	padLen = (index < 56) ? (56 - index) : (120 - index);
-	MD5Update (context, PADDING, padLen);
+	MD5_Update (context, PADDING, padLen);
 
 	/* Append length (before padding) */
-	MD5Update (context, bits, 8);
+	MD5_Update (context, bits, 8);
 
 	/* Store state in digest */
 	Encode (digest, context->state, 16);
